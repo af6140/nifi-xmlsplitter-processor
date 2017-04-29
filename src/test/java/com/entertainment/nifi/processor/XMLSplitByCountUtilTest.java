3c20a690bd6b17877b585395366ca1486b6e05d3
Wang, Dawei <daweiwang.gatekeeper@gmail.com>
2017-04-29 00:10:06 -0400

    test wellformed xml after split.

--- src/test/java/com/entertainment/nifi/processor/XMLSplitByCountUtilTest.java
+++ src/test/java/com/entertainment/nifi/processor/XMLSplitByCountUtilTest.java
@@ -4,6 +4,9 @@ import com.entertainment.nifi.processor.util.XMLSplitByCountUtil;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.List;
@@ -29,7 +32,7 @@ public class XMLSplitByCountUtilTest {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        xmlSplitByCountUtil =new XMLSplitByCountUtil(new File("./target/test-classes").toPath(),in, 1, 4, "aaaaa\n", "bbbbb\n");
+        xmlSplitByCountUtil =new XMLSplitByCountUtil(new File("./target/test-classes").toPath(),in, 1, 4, "<root>", "</root>");
         List<File> files = xmlSplitByCountUtil.split();
         assert (files.size()==3);
         for(File f: files) {
@@ -52,16 +55,31 @@ public class XMLSplitByCountUtilTest {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        xmlSplitByCountUtil =new XMLSplitByCountUtil(new File("./target/test-classes").toPath(),in, 1, 5, "aaaaa\n", "bbbbb\n");
+        xmlSplitByCountUtil =new XMLSplitByCountUtil(new File("./target/test-classes").toPath(),in, 1, 5, "<root>", "</root>");
         List<File> files = xmlSplitByCountUtil.split();
         assert (files.size()==3);
-        for(File f: files) {
-            assert Files.exists(f.toPath());
-//            try {
-//                Files.deleteIfExists(f.toPath());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+        File f =files.get(2);
+        assert testWellFormed(f);
+    }
+
+    private boolean testWellFormed(File xmlFile){
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        boolean wellFormed=false;
+        try {
+            FileInputStream inputStream = new FileInputStream(xmlFile);
+//Instantiate a reader parsing:
+            XMLStreamReader reader = factory.createXMLStreamReader(inputStream);
+
+            while (reader.hasNext()) {
+                //check to be implemented??
+                reader.next();
+            }
+            wellFormed=true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
         }
+        return wellFormed;
     }
 }
